f1b0b4d2d2d507d0484449b1113a3129dce282cb
nick <zhouzhisong@ejuchina.com>
2017-09-22 21:28:33 +0800

    asdasfsdf

---
+++ src/containers/main/index.js
@@ -0,0 +1,28 @@
+import React,{component} from 'react'
+import {Row,Col,Card} from 'antd'
+
+class main extends Component{
+    constructor(props){
+        super(props)        
+    }
+    render(){
+        return(
+            <div>
+                <Row>
+                    <Col>
+                    <Card style={{width:240}} bodyStyle={{padding:0}}>
+                        <div className="team-image">
+                            <img alt="team" width="100%" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                        </div>
+                        <div className="team-card">
+                            <h3>Golden State Warriors</h3>
+                        </div>
+                    </Card>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default main
